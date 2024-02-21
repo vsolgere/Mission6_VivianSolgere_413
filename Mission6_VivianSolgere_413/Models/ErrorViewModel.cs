ff115072434f572722f4f0f20c0a3fac3c41a868
vsolgere <viviansolgere@gmail.com>
2024-02-21 11:09:34 -0700

    Add project files.

---
+++ Mission6_VivianSolgere_413/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Mission6_VivianSolgere_413.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
